088b3f1dd1b82af50c83a91b4723eee43d3a1a7d
GabePiscitelli <112636594+GabePiscitelli@users.noreply.github.com>
2022-09-09 14:54:34 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    double height;
+    int age;
+    String name;
+    String haircolor;
+    //variables for all of the attributes
+    public person(double h, int a, String n, String hC){
+        height = h;
+        age = a;
+        name = n;
+        hairColor = hC;
+    }
+    
+   
+}
